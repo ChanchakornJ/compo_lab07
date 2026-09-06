558866b813a3b627d744c0b4d8fea5110865423f
ChanchakornJ <chanchakornj@gmail.com>
2026-09-07 00:36:08 +0700

    3.3 events/id error

--- src/main/java/se331/lab07/controller/EventController.java
+++ src/main/java/se331/lab07/controller/EventController.java
@@ -110,21 +110,21 @@ public class EventController {
         }
 
     }
-//    @GetMapping("events/{id}")
-//    public ResponseEntity<?> getEvent(@PathVariable("id") Long id){
-//        Event output = null;
-//        for (Event event : eventList){
-//            if (event.getId().equals(id)){
-//                output = event;
-//                break;
-//            }
-//        }
-//        if (output != null){
-//            return ResponseEntity.ok(output);
-//        }else {
-//            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "The given id is not found");
-//        }
-//    }
+    @GetMapping("events/{id}")
+    public ResponseEntity<?> getEvent(@PathVariable("id") Long id){
+        Event output = null;
+        for (Event event : eventList){
+            if (event.getId().equals(id)){
+                output = event;
+                break;
+            }
+        }
+        if (output != null){
+            return ResponseEntity.ok(output);
+        }else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "The given id is not found");
+        }
+    }
 
 }
 
