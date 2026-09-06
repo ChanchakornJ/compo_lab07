c8657ba1258ce905c853b1713368406be30463f3
ChanchakornJ <chanchakornj@gmail.com>
2026-09-07 00:50:53 +0700

    4.3 fix configuring cors

--- src/main/java/se331/lab07/Application.java
+++ src/main/java/se331/lab07/Application.java
@@ -1,25 +1,25 @@
 package se331.lab07;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@Bean
-public WebMvcConfigurer corsConfigurer(){
-    return new WebMvcConfigurer() {
-        @Override
-        public void addCorsMappings(CorsRegistry registry) {
-            registry.addMapping("/events")
-                    .allowedOrigins("https://localhost:5173");
-        }
-    };
-}
-public class Application {
 
+
+public class Application {
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/events")
+                        .allowedOrigins("http://localhost:5173");
+            }
+        };
+
+    }
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
