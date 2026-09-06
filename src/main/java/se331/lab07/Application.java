1690abd2db98ccd162c14c112073c1cde71b9ad5
ChanchakornJ <chanchakornj@gmail.com>
2026-09-07 01:00:39 +0700

    4.4

--- src/main/java/se331/lab07/Application.java
+++ src/main/java/se331/lab07/Application.java
@@ -14,10 +14,12 @@ public class Application {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/events")
-                        .allowedOrigins("http://localhost:5173");
-                registry.addMapping("/events/*")
-                        .allowedOrigins("http://localhost:5173");
+//                registry.addMapping("/events")
+//                        .allowedOrigins("http://localhost:5173");
+//                registry.addMapping("/events/*")
+//                        .allowedOrigins("http://localhost:5173");
+                registry.addMapping("/**")
+                            .allowedOrigins("http://localhost:5173");
             }
         };
 
