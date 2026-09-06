7ff706457803b08643af5c5cf8093a10e0222351
ChanchakornJ <chanchakornj@gmail.com>
2026-09-06 23:24:40 +0700

    3.2 handle indexoutofbound error

--- src/main/java/se331/lab07/controller/EventController.java
+++ src/main/java/se331/lab07/controller/EventController.java
@@ -1,9 +1,12 @@
 package se331.lab07.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 import se331.lab07.entity.Event;
 
 import jakarta.annotation.PostConstruct;
@@ -89,18 +92,39 @@ public class EventController {
                 .organizer("Brody Kill")
                 .build());
     }
+
     @GetMapping("/events")
-    public ResponseEntity<?> getEventLists(@RequestParam(value = "_limit", required= false)Integer perPage
-            ,@RequestParam(value = "_page", required =false) Integer page){
-        perPage = perPage == null?eventList.size() :perPage;
-        page = page == null?1:page;
-        Integer firstIndex = (page-1) *perPage;
+    public ResponseEntity<?> getEventLists(@RequestParam(value = "_limit", required = false) Integer perPage
+            , @RequestParam(value = "_page", required = false) Integer page) {
+        perPage = perPage == null ? eventList.size() : perPage;
+        page = page == null ? 1 : page;
+        Integer firstIndex = (page - 1) * perPage;
         List<Event> output = new ArrayList<>();
-        for(int i = firstIndex; i < firstIndex + perPage; i++) {
-            output.add(eventList.get(i));
+        try {
+            for (int i = firstIndex; i < firstIndex + perPage; i++) {
+                output.add(eventList.get(i));
+            }
+            return ResponseEntity.ok(output);
+        }catch (IndexOutOfBoundsException ex){
+            return ResponseEntity.ok(output);
         }
-        return ResponseEntity.ok(output);
+
     }
+//    @GetMapping("events/{id}")
+//    public ResponseEntity<?> getEvent(@PathVariable("id") Long id){
+//        Event output = null;
+//        for (Event event : eventList){
+//            if (event.getId().equals(id)){
+//                output = event;
+//                break;
+//            }
+//        }
+//        if (output != null){
+//            return ResponseEntity.ok(output);
+//        }else {
+//            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "The given id is not found");
+//        }
+//    }
 
 }
 
