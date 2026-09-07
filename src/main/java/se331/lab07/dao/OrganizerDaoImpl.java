cc7d4ced8cbb5a9705bb001296a2bfc5c48c1b90
ChanchakornJ <Chanchakornj@gmail.com>
2026-09-07 15:42:48 +0700

    5.7 organizer endpoint

---
+++ src/main/java/se331/lab07/dao/OrganizerDaoImpl.java
@@ -0,0 +1,68 @@
+package se331.lab07.dao;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Repository;
+import se331.lab07.entity.Organizer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class OrganizerDaoImpl implements OrganizerDao{
+    List<Organizer> organizerList;
+
+    @PostConstruct
+    public void init(){
+        organizerList = new ArrayList<>();
+        organizerList.add(Organizer.builder()
+                .id(1L)
+                .name("Kat Laydee")
+                .address("123 Main Street")
+                .build());
+
+        organizerList.add(Organizer.builder()
+                .id(2L)
+                .name("Fern Pollin")
+                .address("456 Garden Avenue")
+                .build());
+
+        organizerList.add(Organizer.builder()
+                .id(3L)
+                .name("Carey Wales")
+                .address("789 Beach Road")
+                .build());
+
+        organizerList.add(Organizer.builder()
+                .id(4L)
+                .name("Dawg Dahd")
+                .address("100 Dog Street")
+                .build());
+
+        organizerList.add(Organizer.builder()
+                .id(5L)
+                .name("Kahn Opiner")
+                .address("200 Community Road")
+                .build());
+
+        organizerList.add(Organizer.builder()
+                .id(6L)
+                .name("Brody Kill")
+                .address("300 Highway Road")
+                .build());
+    }
+    @Override
+    public Integer getOrganizerSize(){
+        return organizerList.size();
+    }
+    @Override
+    public List<Organizer> getOrganizers(Integer pageSize, Integer page){
+        pageSize = pageSize == null ? organizerList.size() : pageSize;
+        page = page == null ? 1 : page;
+        int firstIndex=(page -1) * pageSize;
+        return organizerList .subList(firstIndex, Math.min(firstIndex + pageSize, organizerList.size()));
+    }
+    @Override
+    public Organizer getOrganizer(Long id){
+        return organizerList.stream().filter(organizer -> organizer.getId().equals(id)).findFirst().orElse(null);
+    }
+}
