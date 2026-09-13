49d4ca36619bae3e9a8273676c7ce0fd64a16912
ChanchakornJ <Chanchakornj@gmail.com>
2026-09-14 02:43:39 +0700

    4.8 same browser output

--- src/main/java/se331/lab07/controller/EventController.java
+++ src/main/java/se331/lab07/controller/EventController.java
@@ -1,6 +1,7 @@
 package se331.lab07.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,15 +27,13 @@ public class EventController {
     public ResponseEntity<?> getEventLists(@RequestParam(value = "_limit", required = false) Integer perPage
             , @RequestParam(value = "_page", required = false) Integer page) {
 
-        List<Event> output = null;
-        Integer eventSize = eventService.getEventSize();
+        Page<Event> pageOutput = eventService.getEvents(perPage, page);
         HttpHeaders responseHeader = new HttpHeaders();
-        responseHeader.set("x-total-count", String.valueOf(eventSize));
-        try {
-            output = eventService.getEvents(perPage, page);
-            return new ResponseEntity<>(output, responseHeader, HttpStatus.OK);
-        }catch (IndexOutOfBoundsException ex){
-            return new ResponseEntity<>(output, responseHeader, HttpStatus.NOT_FOUND);
+        responseHeader.set("x-total-count", String.valueOf(pageOutput.getTotalElements()));
+        try{
+            return ResponseEntity.ok().headers(responseHeader).body(pageOutput.getContent());
+        }catch(IndexOutOfBoundsException e){
+            return ResponseEntity.ok().headers(responseHeader).body(pageOutput.getContent());
         }
 
     }
